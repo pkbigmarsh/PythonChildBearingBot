8498986872d7dfd538fb2e68e32794a01a2eaf9b
Marshall Sumwalt <marshall.sumwat@perceptivesoftware.com>
2013-07-30 16:52:45 -0500

    Added livelyhood to the children bots, limited the number of children able to be produce(my server kicked me after 2).
    Current problem is that the children don't die properly.

--- child_bearer_bot.py
+++ child_bearer_bot.py
@@ -12,35 +12,48 @@ class ChildBearer(BasicBot):
 		this.isAbleToBear = False;
 		this.childTimer = Timer(10, this.update_child_bearing)
 		this.childTimer.start()
+		this.max_children = 2
 
 	def create_bot(this, msg):
 		print '!! --- Creating Child --- !!'
-		nick = this.parseUser(msg) + "_jr"
+		mommy = this.parseUser(msg)
+		nick = mommy + "_jr"
 		hasChild = False
 		for child in this.children:
 			if child.NICK == nick:
 				hasChild = True
 		if hasChild == False:
 			target, message = this.parsePRIVMSG(msg)
-			child_bot = ChildBot(this.HOST, nick, target)
+			child_bot = ChildBot(this.HOST, nick, target, mommy)
 			this.children.append(child_bot)
 			this.sockets.append(child_bot.sock)
 			child_bot.connect();
+			return True
+		else:
+			return False
 
 	def handle_messages(this, msg):
-		if 'child' in msg and this.isAbleToBear == True:
-			this.create_bot(msg)
-			this.isAbleToBear = False
-			this.childTimer = Timer(10, this.update_child_bearing)
-			this.childTimer.start()
-		elif 'child' in msg and this.isAbleToBear == False:
-			user = this.parseUser(msg)
-			target, message = this.parsePRIVMSG(msg)
+		user = this.parseUser(msg)
+		target, message = this.parsePRIVMSG(msg)
+		if 'child' in msg and this.isAbleToBear == True and len(this.children) < this.max_children:
+			if this.create_bot(msg):
+				this.isAbleToBear = False
+				this.childTimer = Timer(10, this.update_child_bearing)
+				this.childTimer.start()
+			else:
+				this.message(target, user + ' don\'t be greedy')
+		elif 'child' in msg and this.isAbleToBear == False and len(this.children) < this.max_children:
 			this.message(target, user + ' you can\'t have a child');
+		elif 'child' in msg and len(this.children) == this.max_children:
+			this.message(target, 'I\'m sorry ' + user + ', these hips just can\'t handle no more...')
 
 	def update_child_bearing(this):
 		this.isAbleToBear = True
 		this.childTimer.cancel()
+
+	def remove_child(this, child_pos):
+		dead_child = this.children.pop(child_pos)
+		this.sockets.pop(child_pos)
 
 	def run(this):
 		this.sock.connect((this.HOST, this.PORT))
@@ -59,10 +72,13 @@ class ChildBearer(BasicBot):
 					this.handle_messages(line)
 				elif line.find('PING') != -1:
 					this.pingPong(line)
-
+			child_count = 0
 			for child in this.children:
-				if(child.sock in ready_to_read):
+				if child.isAlive == False:
+					this.remove_child(child_count)
+				if(child.sock in ready_to_read and child.isAlive):
 					line2 = child.sock.recv(500)
 					if len(line2) > 0:
 						print 'Child --- ' + child.NICK + '::' + line2
-					child.basic_message_handle(line2)
+					child.handle_message(line2)
+				child_count += 1
--- child_bot.py
+++ child_bot.py
@@ -1,10 +1,143 @@
 from basic_bot import *
-import time
+from threading import Timer
+import random
+import re
 
 class ChildBot(BasicBot):
-	def __init__(this, host, nick, channel_list):
-		BasicBot.__init__(this, host, nick, channel_list)
-		this.lifespan = 100
+	def __init__(this, host, nick, channel, mother):
+		BasicBot.__init__(this, host, nick, channel)
+		this.heartbeat = Timer(5, this.beat_heart)
+		this.heartbeat.start()
+		this.hunger = 0
+		this.thirst = 0
+		this.love = 0
+		this.max_ignore = 3
+		this.mother_nick = mother
+		this.isAlive = True
+		random.seed()
 
-	def handle_messages(this, msg):
-		print this.nick
+	def beat_heart(this):
+		print this.NICK + ' - heartbeat'
+		choice = random.randint(0,2)
+		if choice == 0:
+			this.hunger += 1
+		elif choice == 1:
+			this.thirst += 1
+		elif choice == 2:
+			this.love += 1
+
+		if this.hunger > this.max_ignore:
+			this.death_by_hunger()
+		elif this.thirst > this.max_ignore:
+			this.death_by_thirst()
+		elif this.love > this.max_ignore:
+			this.death_by_love()
+		else:
+			if this.hunger > this.thirst and this.hunger > this.love:
+				this.hunger_message()
+			elif this.thirst > this.hunger and this.thirst > this.love:
+				this.thirst_message()
+			elif this.love > this.hunger and this.love > this.thirst:
+				this.love_message()
+			else:
+				this.random_message()
+		this.heartbeat = Timer(5, this.beat_heart)
+		this.heartbeat.start()
+
+	def random_message(this):
+		choice = random.randint(0,2)
+		print('random message: ', choice)
+		if choice == 0:
+			this.thirst_message()
+		elif choice == 1:
+			this.love_message()
+		elif choice == 2:
+			this.hunger_message()
+
+	def death_by_hunger(this):
+		this.message(this.CHANNEL_LIST, 'Moommmmmmyyyyy I hungyyyy... bye... bye...')
+		this.disconnect()
+
+	def death_by_thirst(this):
+		this.message(this.CHANNEL_LIST, 'I\'m soooo thirsty... bye... bye...')
+		this.disconnect()
+
+	def death_by_love(this):
+		this.message(this.CHANNEL_LIST, 'I guess I might as well run away... Mommy doesn\'t even notice me...')
+		this.disconnect()
+
+	def hunger_message(this):
+		choice = random.randint(0,2)
+		print('Hunger message: ', choice)
+		if choice == 0:
+			this.message(this.CHANNEL_LIST, 'Is there any food')
+		elif choice == 1:
+			this.message(this.CHANNEL_LIST, 'I\'m sooooo hungry... ' + this.mother_nick + ' can I have a snack?')
+		elif choice == 2:
+			this.message(this.CHANNEL_LIST, 'Feed me mommy!!')
+
+	def thirst_message(this):
+		choice = random.randint(0, 2)
+		print('Thirst message: ', choice)
+		if choice == 0:
+			this.message(this.CHANNEL_LIST, 'Is there any drink?')
+		elif choice == 1:
+			this.message(this.CHANNEL_LIST, 'I\'m sooooo thirsy... ' + this.mother_nick + ' can I have a soda? pwease? pwease??')
+		elif choice == 2:
+			this.message(this.CHANNEL_LIST, 'I want a mountain dew!!!')
+
+	def love_message(this):
+		choice = random.randint(0, 2)
+		print('Love message: ', choice)
+		if choice == 0:
+			this.message(this.CHANNEL_LIST, 'Do you love me?')
+		elif choice == 1:
+			this.message(this.CHANNEL_LIST, 'Can I have a hug?')
+		elif choice == 2:
+			this.message(this.CHANNEL_LIST, 'Love me mommy...')
+
+	def disconnect(this):
+		this.sock.close()
+		this.heartbeat.cancel()
+		this.isAlive = False
+
+	def parse(this, msg):
+		user = this.parseUser(msg)
+		target, message = this.parsePRIVMSG(msg)
+
+		food_rgexp = '(food|snack|steak|chips)'
+		thirst_rgexp = '(drink|soda|mtn dew|mountain dew|water|coffee|tea)'
+		love_rgexp = '(love|hug|high five|' + this.NICK + ')'
+
+		if user == this.mother_nick:
+			# Search for food action
+			search = re.compile(food_rgexp, re.I)
+			results = re.search(search, message)
+			if results and this.hunger > 0:
+				this.message(this.CHANNEL_LIST, 'Mommy fed me!! :)')
+				this.hunger -= 1
+
+			# Search for thirst action
+			search = re.compile(thirst_rgexp, re.I)
+			results = re.search(search, message)
+			if results and this.thirst > 0:
+				this.message(this.CHANNEL_LIST, 'I\'m not so thirsy any more mommy :)')
+				this.thirst -= 1
+
+			# Search for love action
+			search = re.compile(love_rgexp, re.I)
+			results = re.search(search, message)
+			if results and this.love > 0:
+				this.message(this.CHANNEL_LIST, 'I love you too mommy :)')
+				this.love -= 1
+
+	def handle_message(this, msg):
+		if len(msg) > 0:
+			print 'Child --- ' + this.NICK + '::' + msg
+		if msg.find("Welcome") != -1:
+			this.join(this.CHANNEL_LIST)
+			this.message(this.CHANNEL_LIST, this.mother_nick + ', hi mommy!!!! :)')
+		elif msg.find("PRIVMSG") != -1:
+			this.parse(msg)
+		elif msg.find('PING') != -1:
+			this.pingPong(msg)
